39c306cfc05fc71a45661b6ae060433b4ee77693
Alekseeva Anna <89023322948@yandex.ru>
2021-04-12 16:01:45 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+"use strict";
+let bet = percent/100/12;
+let firstContribution = parseFloat(contribution);
+let credit = parseFloat(amount);
+let dateEnd = date;
+let totalAmount;
+let currentDate = new Date();
+let quantityOfMonths;
+
+quantityOfMonths = (dateEnd.getFullYear() - currentDate.getFullYear()) * 12 + (dateEnd.getMonth() - currentDate.getMonth());
+totalAmount = (credit - firstContribution) * (bet + bet / (Math.pow((1 + bet),quantityOfMonths) - 1)) * quantityOfMonths;
+totalAmount = parseFloat(totalAmount.toFixed(2));
+console.log(totalAmount);
+
+return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+ "use strict";   
+ let user = name;
+ let greeting;
+
+if (user === "" || user === undefined) {
+   user = "Аноним"; 
+}
+
+ greeting = `Привет, мир! Меня зовут ${user}.`;
+ 
+return greeting;
 }
