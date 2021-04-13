425fbe507fb1d7e228d4aec0eaf9cf8dd757c75c
Alekseeva Anna <89023322948@yandex.ru>
2021-04-13 13:23:07 +0300

    second commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,65 +1,53 @@
-function getResult(a,b,c){
 "use strict";
-   let k1 = a;
-   let k2 = b;
-   let k3 = c;
+function getResult(a,b,c){
+
    let d;
    let x = [];
    
-
-   d =  Math.pow(k2,2) -4 *k1*k3;
+   d =  Math.pow(b,2) -4 *a*c;
    
-   if (d == 0) {
-     x[0] = -k2 /(2 * k1);
+   if (d === 0) {
+     x[0] = -b /(2 * a);
    }
    else  if (d > 0) {
-        x[0] = (-k2 + Math.sqrt(d)) / (2 * k1);
-        x[1] = (-k2 - Math.sqrt(d)) / (2 * k1);
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);
+        x[1] = (-b - Math.sqrt(d)) / (2 * a);
    }
     
     return x;
 }
 
 function getAverageMark(marks) {
-  "use strict";
-   let arrayMarks = marks;
    let averageMark;
-   let Summa = 0;
-   let length;
-   
-   length = arrayMarks.length;
+   let summa = 0;
 
-   if (length === 0) {
+   if (marks.length === 0) {
     averageMark = 0;   
    } 
-   else if (length > 5) {
+   else if (marks.length > 5) {
     console.log("Введено больше 5 оценок");
-    arrayMarks.splice(5,length - 5);
-    length = 5;
+    marks.splice(5,);
    }
 
    if (averageMark !== 0) {
-       for (const Mark of arrayMarks) {
-         Summa = Summa + Mark;  
+       for (const Mark of marks) {
+         summa = summa + Mark;  
        }
-       averageMark = Summa/length; 
+       averageMark =  summa/marks.length;
    }
 
-   
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-  "use strict";    
-let user = name;
-let dateOfBorn = dateOfBirthday;
+  
 let today = new Date(); 
 let age;
 let result;
 
-age = today.getFullYear()-dateOfBorn.getFullYear(); 
+age = today.getFullYear() - dateOfBirthday.getFullYear(); 
 
-result = (age > 18)? `Не желаете ли олд-фэшн, ${user}?` : `Сожалею, ${user}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+result = (age > 18)? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
 
 return result;
 
